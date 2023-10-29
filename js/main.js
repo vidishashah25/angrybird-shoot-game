0ca9aeeb9ac3dfa4e65cf5ddd13c53611a7d4337
Vidisha Shah <shahvidisha1996@gmail.com>
2023-10-29 13:48:51 +0100

    Basic functioning of the game bird shoot & score counting

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 let bullet = 5;
 let count = 0;
+let lifeLine = 1;
 
 function angryBirdShoot(bird){
     bird.style.display = 'none';
-    document.getElementById('score').innerHTML = ++count;
+    document.getElementById('currentScore').innerHTML = ++count;
 }
 
 function bombShoot(bird){
     bird.style.display = 'none';
-    document.getElementById('score').innerHTML = --count;
+    if(!lifeLine)
+    {
+        alert('Game Over')
+    }
+    document.getElementById('currentScore').innerHTML = --count;
+    lifeLine = 0;
+    
 }
