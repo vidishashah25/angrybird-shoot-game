14293e650e77b0fa32d3a2f3bcf232359b215c2b
Vidisha Shah <shahvidisha1996@gmail.com>
2023-10-29 18:42:36 +0100

    Maintaining score, bullets & lifeline

--- js/main.js
+++ js/main.js
@@ -2,18 +2,61 @@ let bullet = 5;
 let count = 0;
 let lifeLine = 1;
 
-function angryBirdShoot(bird){
-    bird.style.display = 'none';
-    document.getElementById('currentScore').innerHTML = ++count;
+let body = document.querySelector("body");
+document.addEventListener("mousemove", function (e) {
+  let circle = document.getElementById("circle");
+  let left = e.offsetX;
+  let top = e.offsetY;
+  circle.style.left = left + "px";
+  circle.style.top = top + "px";
+});
+
+function angryBirdShoot(bird) {
+ if(bullet>=0)
+ {
+    bird.style.display = "none";
+    document.getElementById("currentScore").innerHTML = ++count;
+    if(count===4)
+    {
+        bird.style.display = "none";
+        document.getElementById("currentScore").innerHTML = count;
+        setTimeout(()=>{
+            alert('Yuupiee won the game....')
+        },1000)
+        
+    }
+  
+ }
+    
+  
 }
 
-function bombShoot(bird){
-    bird.style.display = 'none';
-    if(!lifeLine)
-    {
-        alert('Game Over')
+function bombShoot(bird) {
+    if(bullet>=0){
+        bird.style.display = "none";
+        if (!lifeLine) {
+          alert("Game Over");
+        }
+        document.getElementById("currentScore").innerHTML = --count;
+        lifeLine = 0;
+        document.getElementById("currentlifeLine").innerHTML = lifeLine;
     }
-    document.getElementById('currentScore').innerHTML = --count;
-    lifeLine = 0;
+        
     
+  
 }
+
+// document.body.addEventListener("click",()=>{})
+window.addEventListener("click",function(){
+    this.setTimeout(()=>{
+        this.document.getElementById("fire").play();
+    },200)
+    
+    bullet--;
+    document.getElementById("currentBullet").innerHTML = bullet;
+    if(bullet<0)
+    {
+        this.alert('No bullets available...')
+    }
+})
+
